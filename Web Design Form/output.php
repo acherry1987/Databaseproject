71643ea52155cfd80db34bf0e30c814c2a486a06
acherry1987 <andrewcherry.v@gmail.com>
2015-11-01 16:01:16 -0500

    yoyo

--- Web Design Form/output.php
+++ Web Design Form/output.php
@@ -1,9 +1,36 @@
 
 <?php
+ini_set( 'display_errors', 1 );
+ini_set( 'display_startup_errors', 1 );
+
+ $db = new PDO('mysql:host=localhost;dbname=Golf;charset=utf8', 'root', 'root');
+ // sets error mode, which allows errors to be thrown, rather than silently ignored
+ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
 $name = $_POST["name"];
-echo "Your name: " . $name;
+echo "Welcome " . $name;
 
 $email = $_POST["email"];
-echo "Email: " . $email;
+echo "Your username is:" . $email;
+
+$postal = $_POST["postal"];
+$skill = $_POST["skillLevel"];
+$country = $_POST["country"];
+
+
+
+// $db->exec( "INSERT INTO User (Email, Name) VALUES ('$email', '$name')" );
+
+$stmt = $db->prepare("INSERT INTO User (Email, Name, Postal, GolfSkill, Handicap) VALUES (:email, :name, :postal, :skill, :country)");
+$stmt->bindParam(':name', $name);
+$stmt->bindParam(':email', $email);
+$stmt->bindParam(':postal', $postal);
+$stmt->bindParam(':skill', $skill );
+$stmt->bindParam(':country', $country);
+
+$stmt->execute();
+
+
 
 ?>
